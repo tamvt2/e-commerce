1ea653532d6f8f4048253ffb0480d7f45c80ab93
Tam Vũ <tamvt02.gcode@gmail.com>
2024-07-08 09:12:25 +0700

    add vercel.json

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 async function connect() {
 	try {
-		mongoose.connect('mongodb://localhost:27017/e-commerce');
+		mongoose.connect(
+			'mongodb+srv://tamvt02gcode:lN4gBBiPjTjWnOl5@e-commerce.s1djp08.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce'
+		);
 		console.log('Connect successfully!!!');
 	} catch (error) {
 		console.log('Connect failure!!!');
